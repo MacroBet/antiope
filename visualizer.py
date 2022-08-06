182469a866fd2886c9d295f80deb2a860baf595b
Marco Bellia <ing.bellia@gmail.com>
2022-08-06 17:04:39 +0200

    update visualizer

--- visualizer.py
+++ visualizer.py
@@ -1,21 +1,24 @@
+from traceback import print_tb
 import matplotlib.pyplot as plt
 import seaborn as sns
 import pandas as pd
 
-reviews = pd.read_csv("./data/reviews_Digital_Music_5.csv").loc[0:1000]
-# print(reviews.describe())
-reviews = reviews.drop(columns=['unixReviewTime'])
+reviews = pd.read_csv("/Volumes/Extreme SSD/data/reviews_Digital_Music_5.csv")
+
 print(reviews.head())
+print(reviews.describe())
 
 
-groupByAsin = reviews.groupby('asin').describe()
-plt.figure(figsize=(14,6))
-plt.title("groupByAsin")
-sns.lineplot(data=groupByAsin)
-sns.lineplot(data=groupByAsin)
-plt.show()
-# print(groupByAsin)
-
-# # groupByAsin.sort_values(by='count', ascending=False)
+groupByAsin = reviews.groupby('asin')
+print(groupByAsin.describe())
+groupByAsin.sort_values(by='count', ascending=False)
 # groupByReviewer = reviews.groupby('reviewerID').describe().sort_values(by=[('overall', 'count')], ascending=False)
 # print(groupByReviewer)
+
+
+# plt.figure(figsize=(14,6))
+# plt.title("groupByAsin")
+# sns.lineplot(data=groupByAsin)
+# sns.lineplot(data=groupByAsin)
+# plt.show()
+# print(groupByAsin)
